05a3508666b6c76bf2032a509863f85f8959e84d
Anthony <anthonybruno63@yahoo.com>
2020-02-21 20:24:52 -0800

    Building the Plug Board

--- Enigma.java
+++ Enigma.java
@@ -21,61 +21,74 @@
 
 
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner; //Gets user Input
 
 
 public class Enigma {
 
+    //Array to hold switch board values
+    static node [] pb = new node [26];
 
-    public static char plugBoard(char a){
-
-
-        a = rotor(a);
-        return a;
+    //nodes used to connect letters to eachother on the plug board
+    public class node{
+        char Letter; //letter used
+        node connect; //connection to that letter
     }
 
-    public static char rotor(char a){
+
+    //Plugboard should take an input - then output the character it is connected to.
+    // 1 to 1
+    public static void plugBoard(){
+
+        buildBoard();
+        printBoard();
 
 
 
 
-        return a;
     }
 
-    public static void reset(){
-
-        //Reset plug board
-        for(int i = 0; i < 26; i++){
-            for(int j = 0; j < 26; j++){
-
-            }
+    //Method to set up the plug board
+    private static void buildBoard(){
+        int let = 65; //used for assigning letters on the plugboard
+        int con = 25; // used to connect letters to each other
+        for(int i = 0; i < (pb.length / 2); i++){
+            pb[i].Letter = (char) (i + let);
+            pb[i].connect = pb[i + con];
+            let--;
+            con--;
         }
-
     }
-
+    private static void printBoard(){
+        for(int i = 0; i < pb.length; i++){
+            System.out.println("Letter: " + pb[i].Letter +"\nConnection: " + pb[i].connect.Letter);
+        }
+    }
 
 
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in); //Create Scanner object
-        System.out.println("Enter message");
+        //System.out.println("Enter message: ");
 
-        String message = input.nextLine(); //Get string
+        //String message = input.nextLine(); //Get string
+        //message = message.toUpperCase(); //set chars to uppercase
 
-        char[] msg = message.toCharArray(); //convert to char array
-        char[] output = new char[msg.length];
+        //char[] msg = message.toCharArray(); //convert to char array
+        //char[] output = new char[msg.length];
 
-        for(int i = 0; i < msg.length; i++){
-            if(Character.isWhitespace(msg[i])) //Take out spaces (enigma doesnt like spaces)
-                output[i] = ' ';
-            else
-                output[i] = plugBoard(msg[i]);
-        }
+        //for(int i = 0; i < msg.length; i++){
+        //   if(Character.isWhitespace(msg[i])) //Take out spaces (enigma does not like spaces)
+        //       output[i] = ' ';
+        //    else
+                plugBoard();
+        //}
 
-        String out = Arrays.toString(output);
+        //String out = Arrays.toString(output);
 
-        System.out.println("\n"+ out);
+        //System.out.println("\n"+ out);
 
 
     }
