6d882a24924264e1660d73ade1e9ba59488b5bf3
Anthony <anthonybruno63@yahoo.com>
2020-02-25 19:39:13 -0800

    Adding rotor connections
    It looks like there is no algorithm for rotor connections. Starting to just build it straight into its own hashtable. Next option is to setup a file that has the settings and stream it into the hash table

--- rotors.java
+++ rotors.java
@@ -1,7 +1,34 @@
 //This file will handle the rotors of the enigma machine.
 //Each rotor will have a different setting to which it connects to another letter.
-//Later iterations will have the rotors move with every entry.
+//Later iterations of code will have the rotors move with every entry.
+/*
+Entry = ABCDEFGHIJKLMNOPQRSTUVWXYZ (rotor right side)
+        ||||||||||||||||||||||||||
+I     = EKMFLGDQVZNTOWYHXUSPAIBRCJ
+II    = AJDKSIRUXBLHWTMCQGZNPYFVOE
+III   = BDFHJLCPRTXVZNYEIWGAKMUSQO
+IV    = ESOVPZJAYQUIRHXLNFTGKDCMWB
+V     = VZBRGITYUPSDNHLXAWMJQOFECK
+ */
 
+
+import java.util.Hashtable;
 
 public class rotors {
+   private static Hashtable<Character,Character> rotor1 = new Hashtable<>();
+   private static Hashtable<Character,Character> rotor2 = new Hashtable<>();
+   private static Hashtable<Character,Character> rotor3 = new Hashtable<>();
+
+    private static void buildRotor1(){
+        rotor1.put('A','E');
+        rotor1.put('B','K');
+        rotor1.put('C','M');
+        rotor1.put('D','F');
+        rotor1.put('E','L');
+        rotor1.put('F','G');
+        rotor1.put('G','D');
+        rotor1.put('H','Q');
+        rotor1.put('G','D');
+
+    }
 }
